a946b7c81ba081db8c0537ee9d04036483e2b126
Udoka <udochukwukaonyela@gmail.com>
2023-02-26 17:31:13 +0100

    feat: configure rules and extensions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,65 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
+  $schema: 'http://json.schemastore.org/eslintrc',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'airbnb',
+    'airbnb-typescript',
+  ],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
       {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
       },
     ],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'linebreak-style': 0,
+    'no-console': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-param-reassign': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'import/no-cycle': 'off',
+    'arrow-body-style': 'off',
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    '@typescript-eslint/no-empty-interface': 'off',
+    'react/function-component-definition': 'off',
   },
 };
