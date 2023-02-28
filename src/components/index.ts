a132090996ec46f5c32f0cb983d97cb3ba523e7f
Udoka <udochukwukaonyela@gmail.com>
2023-02-28 21:54:50 +0100

    feat: exported components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Hero } from './Hero';
+export { default as Layout } from './Layout';
+export { default as Canvas } from './Canvas';
+export { default as Features } from './Features';
+export { default as CTA } from './CTA';
