adf01a1cdcd69f7dfe01fc8688fb3ef763b5db5c
Udoka <udochukwukaonyela@gmail.com>
2023-02-28 21:53:30 +0100

    feat: created an dexported icons

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as DownArrow } from './DownArrow.icon';
+export { default as Cube } from './Cube.icon';
+export { default as Cloud } from './Cloud.icon';
+export { default as ProgrammersOffice } from './ProgrammersOffice.icon';
+export { default as Dev } from './Dev.icon';
+export { default as Radar } from './Radar.icon';
+export { default as Training } from './Training.icon';
