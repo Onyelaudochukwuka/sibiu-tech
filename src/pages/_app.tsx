4fd1bb0a1eeb66a32c59aff63aa884fd49279c00
Udoka <udochukwukaonyela@gmail.com>
2023-03-08 12:31:54 +0100

    chore: imported react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 import { type AppType } from 'next/dist/shared/lib/utils';
 
 import '../styles/globals.css';
 
-const MyApp: AppType = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp: AppType = ({ Component, pageProps }: any) => <Component {...pageProps} />;
 
 export default MyApp;
